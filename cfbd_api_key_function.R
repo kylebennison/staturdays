482c1453f935acf0fe05be01035e4d0dcd30042d
kylebennison <kylebeni012@gmail.com>
2021-06-05 23:05:44 -0400

    add simple function for authenticating the cfbdata api

---
+++ cfbd_api_key_function.R
@@ -0,0 +1,25 @@
+library(jsonlite)
+library(tidyverse)
+library(httr)
+library(RCurl)
+
+# Prereq: You need to get an API key, and then locate your .Renviron file and add a new line
+# cfbd_staturdays_key = "mysecretkey"
+key <- Sys.getenv("cfbd_staturdays_key")
+
+url <- "https://api.collegefootballdata.com/plays?seasonType=both&year=2020&week=1"
+
+cfbd_api <- function(url, key){
+
+data <- httr::GET(url = url, 
+                  content_type_json(),
+                  add_headers('Authorization' = paste('Bearer', key)))
+
+data <- content(data, as="text", encoding = "UTF-8") %>% 
+  fromJSON(flatten = TRUE) %>% 
+  tibble() %>% 
+  readr::type_convert()
+
+}
+
+week1 <- cfbd_api(url = url, key = key)
