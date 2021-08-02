d10f45df7cbd3205d3f6e5bf7e24989e16066558
kylebennison <kylebeni012@gmail.com>
2021-08-01 23:52:32 -0400

    add elo functions

---
+++ Production/elo_calc_functions.R
@@ -0,0 +1,9 @@
+calc_expected_score <- function(team_rating, opp_team_rating){
+  quotient_home <- 10^((team_rating)/400)
+  quotient_away <- 10^((opp_team_rating)/400)
+  return(expected_score_home <- quotient_home / (quotient_home + quotient_away))
+}
+
+calc_new_elo_rating <- function(team_rating, actual_score, expected_score, k){
+  return(team_rating + k * (actual_score - expected_score))
+}
