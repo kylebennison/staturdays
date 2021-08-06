3f29ceb548f822a1e1dcac13c99bba8139186dd1
Kyle Bennison <kylebeni012@gmail.com>
2021-08-06 15:54:34 -0400

    make week optional, use bind_rows instead

--- Production/get_games_api.R
+++ Production/get_games_api.R
@@ -1,14 +1,5 @@
 # Function to get games from cfbd api
-get_games <- function(start_week, end_week, start_year, end_year){
-  
-  library(scales)
-  library(tidyverse)
-  library(RCurl)
-  library(jsonlite)
-  library(stringr)
-  library(lubridate)
-  library(bit64)
-  library(data.table)
+get_games <- function(start_year, end_year, start_week, end_week){
   
   source("https://raw.githubusercontent.com/kylebennison/staturdays/master/Production/Staturdays%20Colors%20and%20Theme.R")  
   source("https://raw.githubusercontent.com/kylebennison/staturdays/master/Production/cfbd_api_key_function.R")
@@ -16,16 +7,33 @@ get_games <- function(start_week, end_week, start_year, end_year){
   
 base_url_games <- "https://api.collegefootballdata.com/games?" # Base URL for games data
 
-games.master = tibble()
-for (j in start_year:end_year) {
-  for (i in start_week:end_week) {
-    cat('Loading Games', j, 'Week', i, '\n')
-    full_url_games <- paste0(base_url_games, "year=", as.character(j), "&week=", as.character(i), "&seasonType=both")
-    games <- cfbd_api(full_url_games, my_key)
-    games <- as_tibble(games)
-    games.master = rbind(games.master, games)
+if(missing(start_week) | missing(end_week)){
+  
+  games.master = dplyr::tibble()
+  for (j in start_year:end_year) {
+      cat('Loading Games for year ', j, '\n')
+      full_url_games <- paste0(base_url_games, "year=", as.character(j), "&seasonType=both")
+      games <- cfbd_api(full_url_games, my_key)
+      games <- dplyr::as_tibble(games)
+      games.master = dplyr::bind_rows(games.master, games)
+    }
+  
+} else {
+  
+  games.master = dplyr::tibble()
+  for (j in start_year:end_year) {
+    for (i in start_week:end_week) {
+      cat('Loading Games', j, 'Week', i, '\n')
+      full_url_games <- paste0(base_url_games, "year=", as.character(j), "&week=", as.character(i), "&seasonType=both")
+      games <- cfbd_api(full_url_games, my_key)
+      games <- dplyr::as_tibble(games)
+      games.master = dplyr::bind_rows(games.master, games)
+    }
   }
+  
 }
+
+
 
 return(games.master)
 
