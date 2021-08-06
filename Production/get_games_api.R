fee51a13a1b2864e69811d58df3a4f85d3603dc9
Kyle Bennison <kylebeni012@gmail.com>
2021-08-06 13:58:55 -0400

    add required functions and packages

--- Production/get_games_api.R
+++ Production/get_games_api.R
@@ -1,5 +1,19 @@
 # Function to get games from cfbd api
 get_games <- function(start_week, end_week, start_year, end_year){
+  
+  library(scales)
+  library(tidyverse)
+  library(RCurl)
+  library(jsonlite)
+  library(stringr)
+  library(lubridate)
+  library(bit64)
+  library(data.table)
+  
+  source("https://raw.githubusercontent.com/kylebennison/staturdays/master/Production/Staturdays%20Colors%20and%20Theme.R")  
+  source("https://raw.githubusercontent.com/kylebennison/staturdays/master/Production/cfbd_api_key_function.R")
+  source("https://raw.githubusercontent.com/kylebennison/staturdays/master/Production/Play%20Types%20and%20Power%20Conference%20Names.R")
+  
 base_url_games <- "https://api.collegefootballdata.com/games?" # Base URL for games data
 
 games.master = tibble()
