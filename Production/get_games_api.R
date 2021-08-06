02d4910dfa7437862c19fc9c5e06dc8961d7ba25
Kyle Bennison <kylebeni012@gmail.com>
2021-08-06 13:51:25 -0400

    add games api that returns games tibble

---
+++ Production/get_games_api.R
@@ -0,0 +1,18 @@
+# Function to get games from cfbd api
+get_games <- function(start_week, end_week, start_year, end_year){
+base_url_games <- "https://api.collegefootballdata.com/games?" # Base URL for games data
+
+games.master = tibble()
+for (j in start_year:end_year) {
+  for (i in start_week:end_week) {
+    cat('Loading Games', j, 'Week', i, '\n')
+    full_url_games <- paste0(base_url_games, "year=", as.character(j), "&week=", as.character(i), "&seasonType=both")
+    games <- cfbd_api(full_url_games, my_key)
+    games <- as_tibble(games)
+    games.master = rbind(games.master, games)
+  }
+}
+
+return(games.master)
+
+}
