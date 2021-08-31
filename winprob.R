767938224f969e968582ca975c25e8ed40f8bc94
DrewBennison <dhb5140@psu.edu>
2021-08-30 21:17:16 -0400

    clock is bad

--- winprob.R
+++ winprob.R
@@ -13,172 +13,19 @@ library(caret)
 library(xgboost)
 
 source("Production/source_everything.R")
-#library(ggimage)
-
-#Staturdays Colors
-
-staturdays_col_list <- c(
-  lightest_blue = "#5c6272",
-  lighter_blue = "#4c5872",
-  light_blue = "#394871",
-  medium_blue = "#22345a",
-  dark_blue = "#041e42",
-  orange = "#de703b",
-  sign = "#1e1e1e",
-  white = "#FFFFFF"
-)
-
-staturdays_palette <- c("#041e42", "#22345a", "#394871", "#4c5872", "#5c6272", "#de703b")
-
-staturdays_colors <- function(...) {
-  cols <- c(...)
-  
-  if (is.null(cols))
-    return (staturdays_col_list)
-  
-  staturdays_col_list[cols]
-}
-
-staturdays_theme <- theme(plot.caption = element_text(size = 12, hjust = 1, color = staturdays_colors("orange")), 
-                          plot.title = element_text(color = staturdays_colors("dark_blue"), size = 30, face = "bold"),
-                          plot.subtitle = element_text(color = staturdays_colors("lightest_blue"), size = 20),
-                          axis.text = element_text(color = staturdays_colors("lightest_blue"), size = 15),
-                          axis.title = element_text(color = staturdays_colors("lightest_blue"), size = 15),
-                          legend.title = element_text(color = staturdays_colors("lightest_blue"), size = 15),
-                          legend.text = element_text(color = staturdays_colors("lightest_blue"), size = 15)
-)
-
-# Power 5 List
-
-power_5 <- c("ACC", "Big 12", "Big Ten", "Pac-12", "SEC")
-
-scrimmage_plays_all <- 
-  c(
-    "Rush", 
-    "Pass Reception", 
-    "Pass Incompletion", 
-    "Pass Completion", 
-    "Passing Touchdown", 
-    "Rushing Touchdown", 
-    "Sack", 
-    "Pass Interception", 
-    "Pass Interception Return", 
-    "Interception Return Touchdown", 
-    "Fumble Recovery (Own)", 
-    "Fumble Recovery (Opponent)",
-    "Fumble Return Touchdown"
-  )
-
-scrimmage_plays_non_turnover <-
-  c(
-    "Rush", 
-    "Pass Reception", 
-    "Pass Incompletion", 
-    "Pass Completion", 
-    "Passing Touchdown", 
-    "Rushing Touchdown", 
-    "Sack", 
-    "Fumble Recovery (Own)"
-  )
-
-scrimmage_plays_turnover <-
-  c(
-    "Pass Interception", 
-    "Pass Interception Return", 
-    "Interception Return Touchdown", 
-    "Fumble Recovery (Opponent)",
-    "Fumble Return Touchdown",
-    "Safety"
-  )
-
-scrimmage_plays_pass <-
-  c(
-    "Pass Reception", 
-    "Pass Incompletion", 
-    "Pass Completion", 
-    "Passing Touchdown", 
-    "Sack",
-    "Pass Interception", 
-    "Pass Interception Return", 
-    "Interception Return Touchdown"
-  )
-
-scrimmage_plays_rush <-
-  c(
-    "Rush", 
-    "Rushing Touchdown"
-  )
-
-no_action_plays <- 
-  c(
-    "Timeout",
-    "End Period",
-    "End of Half",
-    "End of Game",
-    "Kickoff"
-  )
-
-scrimmage_plays_kicks <- 
-  c(
-    "Punt",
-    "Blocked Punt",
-    "Blocked Punt Touchdown",
-    "Field Goal Missed",
-    "Field Goal Good",
-    "Blocked Field Goal",
-    "Missed Field Goal Return"
-  )
 
 base_url_plays <- "https://api.collegefootballdata.com/plays?" # Base URL to work off of
 base_url_games <- "https://api.collegefootballdata.com/games?" # Base URL for games data
 base_url_drives <- "https://api.collegefootballdata.com/drives?" # Base URL for drives data
 
+#get plays data and gmes data
+plays.master <- get_plays(start_week = 1, end_week = 15, start_year = 2014, end_year = 2020)
+games.master <- get_games(start_week = 1, end_week = 15, start_year = 2014, end_year = 2020)
 
-plays.master <- get_plays(start_week = 1, end_week = 15, start_year = 2019, end_year = 2020)
+#save plays data so as not to call the API each time
+fwrite("C:/Users/drewb/Desktop/cfb_plays_2014_2020.csv")
 
-plays.master = data.frame()
-for (j in 2014:2020) {
-  for (i in 1:15) {
-    cat('Loading Plays', j, 'Week', i, '\n')
-    full_url_plays <- paste0(base_url_plays, "seasonType=both&", "year=", as.character(j), "&","week=", as.character(i)) # Concatenating year and week
-    full_url_plays_encoded <- URLencode(full_url_plays) # If there are spaces in query, formats them correctly
-    plays <- cfbd_api(full_url_plays_encoded,my_key) # Pull in API using url
-    if(is_empty(plays) == F){
-      clockcolumns <- plays %>% unnest_legacy(clock) # Takes clock data out as it's own columns
-      plays <- plays %>%
-        select(-clock) %>%
-        as_tibble() %>%
-        mutate(minutes = clockcolumns$minutes, seconds = clockcolumns$seconds) %>% # Drop old clock dataframe, make a tibble, and add on each individual column of minutes and seconds
-        mutate_at(c("minutes", "seconds"), ~replace(., is.na(.), 0)) # need to turn NAs in clock into 0s
-      plays <- tibble(plays)
-      plays$week = i
-      plays$year = j
-      plays.master = rbind(plays.master, plays, make.row.names=TRUE)
-    }
-  }
-}
-rm(clockcolumns, plays)
-
-games.master = data.frame()
-for (j in 2014:2019) {
-  for (i in 1:15) {
-    cat('Loading Games', j, 'Week', i, '\n')
-    full_url_games <- paste0(base_url_games, "year=", as.character(j), "&week=", as.character(i), "&seasonType=both")
-    full_url_games_encoded <- URLencode(full_url_games)
-    games <- fromJSON(getURL(full_url_games_encoded))
-    games <- as_tibble(games)
-    games.master = rbind(games.master, games)
-  }
-}
-
-plays.master <- read.csv("C:/Users/drewb/Desktop/plays.csv")
-
-# Create a master clock completely in seconds
-plays.master.temp <- plays.master %>% 
-  mutate(clock_in_seconds = 2700-(900*(period-1)) + minutes*60 + seconds)
-
-plays.master <- plays.master.temp
-rm(plays.master.temp)
+plays.master <- read.csv("C:/Users/drewb/Desktop/cfb_plays_2014_2020.csv")
 
 # Remove overtime games
 overtime_games <- plays.master %>% group_by(game_id) %>%
@@ -190,7 +37,8 @@ plays.master <- plays.master %>%
 
 # In-Game Win Probability Model Based on Game State Factors (no Elo) --------------------------
 games.temp <- games.master %>% 
-  select(id, home_team, home_points, away_team, away_points)
+  select(id, home_team, home_points, away_team, away_points) %>% 
+  mutate(id = as.character(id))
 
 plays.master.win_prob <- plays.master %>% mutate(home_score = case_when(home == offense ~ offense_score, # Get home lead/deficit
                                                                         TRUE ~ defense_score),
@@ -200,25 +48,28 @@ plays.master.win_prob <- plays.master %>% mutate(home_score = case_when(home == 
   left_join(games.temp, by = c("home" = "home_team", "away" = "away_team", "game_id" = "id")) # Join games to get final result for each play
 
 # Add win/loss boolean
-plays.master.win_prob3 <- plays.master.win_prob %>% mutate(home_outcome = case_when(home_points > away_points ~ 1,
+plays.master.win_prob2 <- plays.master.win_prob %>% mutate(home_outcome = case_when(home_points > away_points ~ 1,
                                                                                      home_points < away_points ~ 0,
                                                                                      TRUE ~ 0.5))
 
 # Add home possession flag if they have the ball or not
-plays.master.win_prob3 <- plays.master.win_prob3 %>% 
+plays.master.win_prob2 <- plays.master.win_prob2 %>% 
   mutate(home_poss_flag = if_else(home == offense, 1, 0),
          home_timeouts = if_else(home == offense, offense_timeouts, defense_timeouts),
          away_timeouts = if_else(away == offense, offense_timeouts, defense_timeouts))
 
+
+rm(plays.master)
+rm(plays.master.win_prob)
+
 #elo ratings
-elo_ratings <- read_csv(file = "https://raw.githubusercontent.com/kylebennison/staturdays/master/elo_ratings_historic.csv",
-                        col_types = list(col_character(), col_character(), col_double(), col_integer(), col_integer(), col_date(format = "%Y-%m-%d"))) %>% 
+elo_ratings <- read_csv(file = "https://raw.githubusercontent.com/kylebennison/staturdays/master/Production/elo_ratings_historic.csv") %>% 
   select(team, elo_rating, week, season)
 
 elo_ratings_adj <- elo_ratings %>% mutate(week = week + 1)
 
-# Having an issue here where i end up with more rows than before. Join keys may not be unique i.e. multiple matches on rhs for certain plays on lhs
-plays.master.win_prob4 <- plays.master.win_prob3 %>% left_join(elo_ratings, by = c("home" = "team", "week", "year" = "season")) %>% 
+# Having an issue here where I end up with more rows than before. Join keys may not be unique i.e. multiple matches on rhs for certain plays on lhs
+plays.master.win_prob3 <- plays.master.win_prob2 %>% left_join(elo_ratings, by = c("home" = "team", "week", "year" = "season")) %>% 
   rename(home_elo = elo_rating) %>% 
   left_join(elo_ratings, by = c("away" = "team", "week", "year" = "season")) %>% 
   rename(away_elo = elo_rating) %>% 
@@ -226,29 +77,38 @@ plays.master.win_prob4 <- plays.master.win_prob3 %>% left_join(elo_ratings, by =
 
 
 # Add home_elo_diff
-plays.master.win_prob4 <- plays.master.win_prob4 %>% 
+plays.master.win_prob3 <- plays.master.win_prob3 %>% 
   mutate(home_elo_diff = home_elo - away_elo)
 
 
 ### TO DO: remove all plays except the last when there are duplicate times ####
-plays.master.win_prob4 <- plays.master.win_prob4 %>% group_by(game_id, clock_in_seconds) %>% 
+plays.master.win_prob3 <- plays.master.win_prob3 %>% group_by(game_id, clock_in_seconds) %>% 
   filter(row_number()==n()) %>% 
   ungroup()
+
+plays.make.end.rows <- plays.master.win_prob3 %>% 
+  group_by(game_id) %>% 
+  filter(row_number()==n()) %>% 
+  ungroup()
+
+plays.master.win_prob4 <- plays.master.win_prob3 %>% 
+  mutate(game_over = ifelse(clock_in_seconds==0,1,0))
+  
 
 # Prep Data for Modeling 
 y.train <- plays.master.win_prob4$home_outcome
 x.train <- plays.master.win_prob4 %>% select(home_score_lead_deficit, clock_in_seconds, down, distance,
-                                               yards_to_goal, home_poss_flag, home_timeouts,away_timeouts, home_elo_diff) %>% 
+                                               yards_to_goal, home_poss_flag, home_timeouts,away_timeouts, home_elo_diff, game_over) %>% 
   as.matrix()
 
 #extra columns that aren't used for modeling but might be good for prediction
 x.train.leftover <- plays.master.win_prob4 %>% select(-home_score_lead_deficit, -clock_in_seconds, -down, -distance,
-                                                      -yards_to_goal, -home_poss_flag, -home_timeouts,-away_timeouts, -home_elo_diff)
+                                                      -yards_to_goal, -home_poss_flag, -home_timeouts,-away_timeouts, -home_elo_diff, -game_over)
 
 #pick one game if you want to test
-x.test <- plays.master.win_prob4 %>% filter(year == 2019, home == "Wisconsin", away == "Ohio State") %>% 
+x.test <- plays.master.win_prob4 %>% filter(year == 2020, home == "Penn State", away == "Ohio State") %>% 
   select(home_score_lead_deficit, clock_in_seconds, down, distance,
-         yards_to_goal, home_poss_flag, home_timeouts,away_timeouts, home_elo_diff) %>% 
+         yards_to_goal, home_poss_flag, home_timeouts,away_timeouts, home_elo_diff, game_over) %>% 
   as.matrix()
 
 
@@ -294,7 +154,7 @@ x.train.copy$actualhomeresults <- y.train
 res <- cbind(x.train.copy, x.train.leftover)
 
 #plot any game
-x <- res %>% filter(year==2019, week==14, home=="Nebraska", away=="Iowa") %>% 
+x <- res %>% filter(year==2020,home=="Michigan", away=="Penn State") %>% 
   ggplot(aes(x=-clock_in_seconds, y=winprob)) +geom_line() + #rollmean(winprob, 5, na.pad = TRUE))
   ylim(0,1)
 
