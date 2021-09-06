c74c842e059e9092fdd90eea9959399d1d149aa6
kylebennison <kylebeni012@gmail.com>
2021-09-05 22:33:11 -0400

    add table for RBs

--- WIP/qb_ppa_leaders_season.r
+++ WIP/qb_ppa_leaders_season.r
@@ -77,3 +77,73 @@ webshot2::webshot(html, file = paste0("R Plots/qb_ppa_",
                                       lubridate::today(),
                                       ".png"),
                  delay = 1)
+
+
+# RBs ---------------------------------------------------------------------
+
+rb_df <- player_ppa %>% 
+  left_join(colors, by = c("team" = "school")) %>% 
+  filter(position == "RB",
+         countablePlays >= 15) %>% 
+  select(name, light, averagePPA.pass, averagePPA.rush, averagePPA.thirdDown, averagePPA.all)
+
+reactable_tbl <- reactable::reactable(rb_df,
+                                      columns = list(
+                                        name = colDef(name = "Player"),
+                                        light = colDef(name = "Team",
+                                                       cell = function(value) {
+                                                         image <- htmltools::img(src = value, height = "50px", alt = "")
+                                                         htmltools::tagList(
+                                                           htmltools::div(style = list(display = "inline-block", width = "25px"), 
+                                                                          image)
+                                                         )
+                                                       }),
+                                        averagePPA.pass = colDef(name = "Passing",
+                                                                 format = colFormat(digits = 3)),
+                                        averagePPA.rush = colDef(name = "Rushing",
+                                                                 format = colFormat(digits = 3)),
+                                        averagePPA.thirdDown = colDef(name = "3rd Downs",
+                                                                      format = colFormat(digits = 3)),
+                                        averagePPA.all = colDef(name = "All Plays",
+                                                                format = colFormat(digits = 3),
+                                                                style = function(value) {
+                                                                  normalized <- (value - min(rb_df$averagePPA.all)) / (max(rb_df$averagePPA.all) - min(rb_df$averagePPA.all))
+                                                                  color <- orange_pal(normalized)
+                                                                  list(background = color)
+                                                                })
+                                      ),
+                                      columnGroups = list(
+                                        colGroup(name = "Average PPA",
+                                                 columns = c("averagePPA.pass",
+                                                             "averagePPA.rush",
+                                                             "averagePPA.thirdDown",
+                                                             "averagePPA.all"))
+                                      ),
+                                      theme = reactableTheme(
+                                        style = list(fontFamily = "-apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif")),
+                                      defaultSortOrder = "desc",
+                                      defaultSorted = c("averagePPA.all"),
+                                      defaultColDef = colDef(format = colFormat(digits = 3)),
+                                      searchable = TRUE,
+                                      minRows = 30,
+                                      defaultPageSize = 30,
+                                      pagination = FALSE,
+                                      striped = TRUE
+)
+
+t2 <- htmlwidgets::prependContent(reactable_tbl,
+                                  htmltools::h1(class = "title",
+                                                "Top RBs in PPA Through Week 1",
+                                                style = "text-align:center; font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif;"))
+
+t3 <- htmlwidgets::appendContent(t2,
+                                 htmltools::p(class = "footer",
+                                              "@kylebeni012 for @staturdays | Data: @cfb_data",
+                                              style = "color: #de703b; text-align:left; font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Helvetica, Arial, sans-serif;"))
+
+html <- "R Plots/t3.html"
+htmlwidgets::saveWidget(t3, html)
+webshot2::webshot(html, file = paste0("R Plots/rb_ppa_",
+                                      lubridate::today(),
+                                      ".png"),
+                  delay = 1)
