7c224d5b9c49f139b09c66d7adcba54ddbb9df41
kylebennison <kylebeni012@gmail.com>
2021-09-21 23:09:59 -0400

    update to full path

--- Production/get_elo.r
+++ Production/get_elo.r
@@ -3,7 +3,7 @@
 
 get_elo <- function(start_year = 2021, end_year = 2021, teams){
   
-  elo <- data.table::fread("Production/elo_ratings_historic.csv", encoding = "UTF-8")
+  elo <- data.table::fread("https://raw.githubusercontent.com/kylebennison/staturdays/master/Production/elo_ratings_historic.csv", encoding = "UTF-8")
   
   elo <- elo %>%
     dplyr::filter(season >= start_year & season <= end_year)
