c8dc5ade21516389991a9d4548f3b6df6686ddb2
kylebennison <kylebeni012@gmail.com>
2021-10-20 21:30:59 -0400

    helper function with ggsave to limit manual input

---
+++ Production/plot_save.r
@@ -0,0 +1,21 @@
+plot_save <- function(plot = last_plot(), filename, filetype = "jpg", size = "horizontal"){
+  
+  #' Saves plot
+  #' plot: a ggplot object
+  #' filename: the name of the file. today's date will be appended to the front
+  #' filetype: either "jpg" or "png"
+  #' size: either "horizontal" or "square"
+  
+  ggsave(filename = paste0(lubridate::today(),
+                           "_",
+                           filename,
+                           ".",
+                           filetype),
+         plot = plot,
+         path = "R Plots/",
+         width = if(size == "square") 200 else 400,
+         height = 200,
+         units = "mm",
+         dpi = 300)
+  
+}
