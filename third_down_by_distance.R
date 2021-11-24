c899edc890a01d5c4018f20064093ff0af3d6c29
DrewBennison <dhb5140@psu.edu>
2021-11-24 09:57:32 -0500

    updated graph title

--- third_down_by_distance.R
+++ third_down_by_distance.R
@@ -117,8 +117,8 @@ master %>% filter(!is.na(bucket), conference %in% c("SEC", "ACC", "Pac-12", "Big
   #geom_text(aes(label=ifelse(above_league_average.x>.2 | above_league_average.x < -0.2,as.character(offense_play),'')),hjust=0,vjust=0) +
   #geom_label_repel(aes(label=ifelse(above_league_average.x>.2,as.character(offense_play),''))) +
   labs(title = "Average distance to go on 3rd downs\nabove/below average vs.\n3rd down conversion rate by\ndistance above/below average",
-    x="3rd down conversion rate above/below league average",
-       y="3rd down yards to go above/below league average at that distance")
+    x="3rd down conversion rate above/below league average at that bucket",
+       y="3rd down yards to go above/below league average at that bucket")
 
 ggsave("C:/Users/drewb/Desktop/mainplot.png", height = 15, width = 5)
 
