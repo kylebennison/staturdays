f97db4dd8410c3cf6794f78fe8cbf0df7f937a5e
Drew Bennison <drewbennison11@gmail.com>
2022-11-08 15:43:18 -0500

    Added folder to store data for article

--- Production/Tweet_Scraping/tweet_scraper.py
+++ Production/Tweet_Scraping/tweet_scraper.py
@@ -1,7 +1,5 @@
 import tweepy
-import csv
 import pandas as pd
-import time
 import os
 
 # Read in OAuth token from environment variable
@@ -11,6 +9,13 @@ token = os.environ.get('tweepy_auth')
 auth = tweepy.OAuth2BearerHandler(token)
 api = tweepy.API(auth)
 
-tweets = tweepy.Cursor(api.search_tweets, q="College", lang="en", tweet_mode='extended').items(2)
+
+# Store and scrape tweets
+dt = []
+tweets = tweepy.Cursor(api.search_tweets, q="#CollegeGameday -filter:retweets Pat -Kirk -Lee -Chris", lang="en", tweet_mode='extended').items(100)
 for tweet in tweets:
-    print(tweet._json['full_text'])
+	dt2 = pd.DataFrame(data={"Tweet": [tweet._json['full_text']]})
+	dt.append(dt2)
+
+dt = pd.concat(dt)
+dt.to_csv("C:/Users/drewb/OneDrive/Documents/Staturdays/Archive/Articles/College Gameday Sentiment/gameday_tweets.csv")
