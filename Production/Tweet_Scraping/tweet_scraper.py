74b15a14f72854a4521e5873a49b6691d21402a6
Drew Bennison <drewbennison11@gmail.com>
2022-11-11 15:31:10 -0500

    scrape tweets for multiple hosts

--- Production/Tweet_Scraping/tweet_scraper.py
+++ Production/Tweet_Scraping/tweet_scraper.py
@@ -1,6 +1,7 @@
 import tweepy
 import pandas as pd
 import os
+import time
 
 # Read in OAuth token from environment variable
 token = os.environ.get('tweepy_auth')
@@ -9,13 +10,41 @@ token = os.environ.get('tweepy_auth')
 auth = tweepy.OAuth2BearerHandler(token)
 api = tweepy.API(auth)
 
-
-# Store and scrape tweets
+n_hours = 1
+n_increments = n_hours * 4
+increments = 0
 dt = []
-tweets = tweepy.Cursor(api.search_tweets, q="#CollegeGameday -filter:retweets Pat -Kirk -Lee -Chris", lang="en", tweet_mode='extended').items(100)
-for tweet in tweets:
-	dt2 = pd.DataFrame(data={"Tweet": [tweet._json['full_text']]})
-	dt.append(dt2)
+
+while increments < n_increments:
+
+	# Store and scrape tweets
+	tweets = tweepy.Cursor(api.search_tweets, q="#CollegeGameday -filter:retweets Pat -Kirk -Lee -Rece", lang="en", tweet_mode='extended').items(100)
+	for tweet in tweets:
+		dt2 = pd.DataFrame(data={"Tweet": [tweet._json['full_text']],
+								"Host": ["Pat"]})
+		dt.append(dt2)
+
+	tweets = tweepy.Cursor(api.search_tweets, q="#CollegeGameday -filter:retweets -Pat Kirk -Lee -Rece", lang="en", tweet_mode='extended').items(100)
+	for tweet in tweets:
+		dt2 = pd.DataFrame(data={"Tweet": [tweet._json['full_text']],
+								"Host": ["Kirk"]})
+		dt.append(dt2)
+
+	tweets = tweepy.Cursor(api.search_tweets, q="#CollegeGameday -filter:retweets -Pat -Kirk Lee -Rece", lang="en", tweet_mode='extended').items(100)
+	for tweet in tweets:
+		dt2 = pd.DataFrame(data={"Tweet": [tweet._json['full_text']],
+								"Host": ["Lee"]})
+		dt.append(dt2)
+
+	tweets = tweepy.Cursor(api.search_tweets, q="#CollegeGameday -filter:retweets -Pat -Kirk -Lee Rece", lang="en", tweet_mode='extended').items(100)
+	for tweet in tweets:
+		dt2 = pd.DataFrame(data={"Tweet": [tweet._json['full_text']],
+								"Host": ["Rece"]})
+		dt.append(dt2)
+
+	print("Completed increment {}".format(increments+1))
+	increments += 1
+	time.sleep(15*60)
 
 dt = pd.concat(dt)
 dt.to_csv("C:/Users/drewb/OneDrive/Documents/Staturdays/Archive/Articles/College Gameday Sentiment/gameday_tweets.csv",
