a185ed4d1f864ec2b9df404ea1b0738baa5aa963
kylebennison <kylebeni012@gmail.com>
2023-01-08 21:40:21 -0500

    working model

--- Production/NFL/predict_fantasy_points/predict_playoff_fantasy_points/predict_playoff_fantasy_points.r
+++ Production/NFL/predict_fantasy_points/predict_playoff_fantasy_points/predict_playoff_fantasy_points.r
@@ -1,4 +1,4 @@
-# Predict postseason fantasy points using regular season fantasy points
+# Predict postseason fantasy points using reguLA season fantasy points
 
 library(nflfastR)
 library(nflreadr)
@@ -8,13 +8,47 @@ library(xgboost)
 library(mltools)
 library(data.table)
 
-seasons <- c(seq(2015, 2021))
+seasons <- c(seq(2010, 2021))
 POSITIONS <- c("QB", "WR", "TE", "RB", "FB", "K", "HB")
 
+# Playoff teams
+teams_2010 <- c("NE", "PIT", "IND", "BAL", "NYJ", "KC", "ATL", "CHI", "GB", "NO", "SEA", "PHI")
+teams_2011 <- c("NE", "BAL", "HOU", "DEN", "PIT", "CIN", "NYG", "SF", "NO", "GB", "DET", "ATL")
+teams_2012 <- c("NE", "BAL", "HOU", "DEN", "IND", "CIN", "ATL", "SF", "GB", "WAS", "MIN", "SEA")
+teams_2013 <- c("NE", "DEN", "IND", "CIN", "KC", "LAC", "SEA", "CAR", "SF", "NO", "PHI", "GB")
+teams_2014 <- c("NE", "DEN", "IND", "CIN", "PIT", "BAL", "SEA", "GB", "DAL", "DET", "CAR", "ARI")
+teams_2015 <- c("NE", "DEN", "CIN", "HOU", "KC", "PIT", "CAR", "ARI", "MIN", "WAS", "GB", "SEA")
+teams_2016 <- c("NE", "LV", "PIT", "HOU", "KC", "MIA", "DAL", "DET", "SEA", "ATL", "NYG", "GB")
+teams_2017 <- c("NE", "PIT", "JAX", "KC", "TEN", "BUF", "PHI", "MIN", "NO", "LA", "CAR", "ATL")
+teams_2018 <- c("KC", "NE", "HOU", "BAL", "LAC", "IND", "NO", "LA", "CHI", "DAL", "SEA", "PHI")
+teams_2019 <- c("BAL", "KC", "NE", "HOU", "BUF", "TEN", "SF", "GB", "NO", "SEA", "MIN", "PHI")
+teams_2020 <- c("KC", "BUF", "PIT", "TEN", "IND", "CLE", "GB", "NO", "SEA", "WAS", "TB", "LA", "BAL", "CHI") # missing BAL, CHI, 
+teams_2021 <- c("BUF", "NE", "CIN", "PIT", "TEN", "KC", "LV", "DAL", "PHI", "GB", "TB", "LA", "ARI", "SF")
+
 # Test season
+# TODO: since we will know which players/teams make the playoffs, let's train only
+# on players who make the playoffs. Need to get playoff T/F for each player's team
+# and filter for T
 
 nfl_stats_season <- nflreadr::load_player_stats(seasons = seasons) %>% 
-  filter(position %in% POSITIONS)
+  filter(position %in% POSITIONS) %>% 
+  mutate(
+    made_playoffs = case_when(
+      season == 2010 ~ if_else(recent_team %in% teams_2010, TRUE, FALSE),
+      season == 2011 ~ if_else(recent_team %in% teams_2011, TRUE, FALSE),
+      season == 2012 ~ if_else(recent_team %in% teams_2012, TRUE, FALSE),
+      season == 2013 ~ if_else(recent_team %in% teams_2013, TRUE, FALSE),
+      season == 2014 ~ if_else(recent_team %in% teams_2014, TRUE, FALSE),
+      season == 2015 ~ if_else(recent_team %in% teams_2015, TRUE, FALSE),
+      season == 2016 ~ if_else(recent_team %in% teams_2016, TRUE, FALSE),
+      season == 2017 ~ if_else(recent_team %in% teams_2017, TRUE, FALSE),
+      season == 2018 ~ if_else(recent_team %in% teams_2018, TRUE, FALSE),
+      season == 2019 ~ if_else(recent_team %in% teams_2019, TRUE, FALSE),
+      season == 2020 ~ if_else(recent_team %in% teams_2020, TRUE, FALSE),
+      season == 2021 ~ if_else(recent_team %in% teams_2021, TRUE, FALSE),
+      TRUE ~ FALSE
+  )) %>% 
+  filter(made_playoffs == TRUE)
 
 # Get each player's total epa for the season
 y <- nfl_stats_season %>% 
@@ -113,11 +147,39 @@ cv <- xgb.cv(
   early_stopping_rounds = 25,
 )
 
-cv$evaluation_log
+# cv$evaluation_log
+cv$best_iteration
+paste0("Best mean test rmse: ", cv$evaluation_log$test_rmse_mean %>% min())
+paste0("Best sd test rmse: ", cv$evaluation_log$test_rmse_std[cv$best_iteration])
+
+X_test <- X %>% filter(ttv == "test") %>% select(!ttv)
+y_test <- y %>% filter(ttv == "test") %>% ungroup() %>% pull("postseason_ppr")
+
+dtest <- xgboost::xgb.DMatrix(as.matrix(X_test), label = y_test)
+
+watchlist <- list(train=X_gb, test=dtest)
 
 xgbmodel <- xgb.train(
   params = params,
   data = X_gb,
   nrounds = cv$best_iteration,
-  early_stopping_rounds = 25
+  early_stopping_rounds = 25,
+  watchlist = watchlist
 )
+
+# Predict on test
+y_preds <- predict(xgbmodel, newdata = dtest)
+
+X_test$pred <- y_preds
+
+X_test$act <- y_test
+
+X_test <- cbind(X_test, extra_x %>% filter(ttv == "test"))
+
+# Predict on validation
+
+# Select team based on predicted points (high to low, one team each and position limits)
+
+# Compare predicted best team to actual best team
+
+# Predict and pick team for 2022
