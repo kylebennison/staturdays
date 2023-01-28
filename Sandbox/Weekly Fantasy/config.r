6577234b1050c4e4238bba2dbf068d9a48137c80
kylebennison <kylebeni012@gmail.com>
2023-01-28 14:55:46 -0500

    add config file, get kicker full names

---
+++ Sandbox/Weekly Fantasy/config.r
@@ -0,0 +1,6 @@
+# List of fantasy positions
+POSITIONS <- c("QB", "RB", "WR", "TE", "K", "DEF")
+
+# BigQuery configs
+PROJECT <- "staturdays"
+DATASET <- "fantasy"
--- Sandbox/Weekly Fantasy/fantasy_points_to_bq.R
+++ Sandbox/Weekly Fantasy/fantasy_points_to_bq.R
@@ -1,22 +1,27 @@
 library(nflreadr)
 library(bigrquery)
+
 # TODO: Use service account
-
-POSITIONS <- c("QB", "RB", "WR", "TE", "K", "DEF")
-PROJECT <- "staturdays"
-DATASET <- "fantasy"
-
+source("config.r")
 
 # Confirm it's time to update the table
 current_week <- get_current_week()
 
-query <- "SELECT MAX(week) AS max_week FROM `staturdays.fantasy.player_points`"
+query <- glue::glue(
+  "SELECT MAX(week) AS max_week FROM `{PROJECT}.{DATASET}.player_points`"
+)
 
 result <- bq_project_query(PROJECT, query)
 max_week <- bq_table_download(result)
 max_week <- max_week$max_week
 
 if(current_week > max_week){
+  
+  # Load in player database to get IDs and full names
+  players <- load_players()
+  
+  players <- players %>% 
+    select(display_name, gsis_id)
 
   # Get fantasy data
   df <- nflreadr::load_player_stats()
@@ -31,12 +36,17 @@ if(current_week > max_week){
   
   kicking <- load_player_stats(stat_type = "kicking")
   
+  # Get kicker full names
   kicking <- kicking %>% 
-    select(player_id, player_name, team, season, week, fg_made, pat_made) %>% 
+    left_join(players, by = c("player_id" = "gsis_id"))
+  
+  kicking <- kicking %>% 
+    select(player_id, display_name, team, season, week, fg_made, pat_made) %>% 
     filter(week == max(week)) %>% 
     mutate(position = "K",
            fantasy_points = fg_made * 3 + pat_made) %>% 
-    select(!c(fg_made,))
+    select(!c(fg_made, pat_made)) %>% 
+    rename(player_name = display_name)
   
   df <- df %>% rbind(kicking)
   
